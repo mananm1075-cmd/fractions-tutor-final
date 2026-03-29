92c2406adf9ef2efd0cbffc1a9df7cd6c40f2dff
mananm1075-cmd <mananm1075@gmail.com>
2026-03-29 21:44:52 +0530

    fix wildcard route for render

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ app.use("/api", tutorRoutes);
 app.use(express.static(path.join(__dirname, "dist")));
 
 // Catch all → React app
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
